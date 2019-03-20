b992dfaacc5387aeb5b6696e2bfc5bfcde28295c
diluculo <diluculo@gmail.com>
2019-03-20 19:46:05 +0900

    Updates

---
+++ precache-manifest.4189b68595401798818e757aacc3bff2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "be4d5475bbdc9e73618f",
+    "url": "/zivelab-app-channels/static/js/runtime~main.041d34bd.js"
+  },
+  {
+    "revision": "aef83d0ff9800df40a12",
+    "url": "/zivelab-app-channels/static/js/main.79e7f21d.chunk.js"
+  },
+  {
+    "revision": "dfaa33fadba568dbb5ba",
+    "url": "/zivelab-app-channels/static/js/2.ed0c5a92.chunk.js"
+  },
+  {
+    "revision": "ae038dac7df498e1dff8909c28bd9270",
+    "url": "/zivelab-app-channels/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/zivelab-app-channels/precache-manifest.52261b2e73c5b8432daa15d1f1f54712.js"
+  "/zivelab-app-channels/precache-manifest.4189b68595401798818e757aacc3bff2.js"
 );
 
 workbox.clientsClaim();
