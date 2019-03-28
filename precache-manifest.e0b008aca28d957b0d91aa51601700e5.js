684bc61d11420316940dee5d2f8f12958e82856b
diluculo <diluculo@gmail.com>
2019-03-28 19:53:52 +0900

    Updates

---
+++ precache-manifest.e0b008aca28d957b0d91aa51601700e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "084dd3faee282a12e037df758f22b654",
+    "url": "/zivelab-channels/static/media/gettingStarted.084dd3fa.md"
+  },
+  {
+    "revision": "b5a7163a3f12e0f4b668",
+    "url": "/zivelab-channels/static/js/runtime~main.149fc12e.js"
+  },
+  {
+    "revision": "4e634001650b753005dc",
+    "url": "/zivelab-channels/static/js/main.73ee8910.chunk.js"
+  },
+  {
+    "revision": "35d11c3d3f4fd1514341",
+    "url": "/zivelab-channels/static/js/2.b5029656.chunk.js"
+  },
+  {
+    "revision": "35d11c3d3f4fd1514341",
+    "url": "/zivelab-channels/static/css/2.ac9ad68d.chunk.css"
+  },
+  {
+    "revision": "8e4fe6e52611f31f84e81e250826c959",
+    "url": "/zivelab-channels/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/zivelab-channels/precache-manifest.77a417cb2bd4dbf94e31afb3943e06c1.js"
+  "/zivelab-channels/precache-manifest.e0b008aca28d957b0d91aa51601700e5.js"
 );
 
 workbox.clientsClaim();
